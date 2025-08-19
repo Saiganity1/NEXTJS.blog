65966331f6cece8a884cc9b876e87e0149432756
Saiganity1 <sicatmichaeldave0411@gmail.com>
2025-08-19 13:51:44 +0800

    Updated Description

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I am Michael Dave Sicat. I am a BSIT-3A Student at University of the Assumptions. My hoobie are playing Basketball and playing games</p>
+        <p>Hello I am Michael Dave Sicat. I am a BSIT-3A Student at University of the Assumptions. My hoobie are playing Basketball and playing games. In 5 years i see my self as a Software Engineer working in a top level company and living a luxury life</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
